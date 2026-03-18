a84d9a7eed1cf1eb78a0b31bbc0e6ae8d58cf5af
zudsie <zudsie>
2026-03-17 20:44:41 -0400

    creating processed volumes

--- setup/003_volumes.sql
+++ setup/003_volumes.sql
@@ -1,2 +1,6 @@
 CREATE VOLUME IF NOT EXISTS osrs_analytics_dev.raw.landing;
 CREATE VOLUME IF NOT EXISTS osrs_analytics_prod.raw.landing;
+
+CREATE VOLUME IF NOT EXISTS osrs_analytics_dev.raw.processed;
+CREATE VOLUME IF NOT EXISTS osrs_analytics_prod.raw.processed;
+
