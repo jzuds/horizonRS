c5f6b582972824b6d3fbb7dc8db6840c7ef43ff1
zudsie <zudsie>
2026-06-23 21:22:58 -0400

    starting back at ingestion

--- setup/003_volumes.sql
+++ setup/003_volumes.sql
@@ -1,6 +1,2 @@
 CREATE VOLUME IF NOT EXISTS osrs_analytics_dev.raw.landing;
 CREATE VOLUME IF NOT EXISTS osrs_analytics_prod.raw.landing;
-
-CREATE VOLUME IF NOT EXISTS osrs_analytics_dev.raw.processed;
-CREATE VOLUME IF NOT EXISTS osrs_analytics_prod.raw.processed;
-
